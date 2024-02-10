89cdfdb9212a8cdb75b7d112e1f641623a639536
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 15:26:32 +1300

    WIP

--- src/Blocks/ServicesBlock.php
+++ src/Blocks/ServicesBlock.php
@@ -3,11 +3,14 @@
 namespace Goldfinch\Component\Services\Blocks;
 
 use DNADesign\Elemental\Models\BaseElement;
+use Goldfinch\Helpers\Traits\BaseElementTrait;
 use Goldfinch\Component\Services\Models\Nest\ServiceItem;
 use Goldfinch\Component\Services\Models\Nest\ServiceCategory;
 
 class ServicesBlock extends BaseElement
 {
+    use BaseElementTrait;
+
     private static $table_name = 'ServicesBlock';
     private static $singular_name = 'Services';
     private static $plural_name = 'Services';
